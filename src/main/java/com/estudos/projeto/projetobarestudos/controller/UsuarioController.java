04f22d514fb0c7d26dfbbe72f5d0df815fd0fbd0
jvscJoao <joaov1500@gmail.com>
2023-12-20 16:31:34 -0300

    Buscar todos os Cliente

---
+++ src/main/java/com/estudos/projeto/projetobarestudos/controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package com.estudos.projeto.projetobarestudos.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudos.projeto.projetobarestudos.domain.Usuario.Usuario;
+import com.estudos.projeto.projetobarestudos.services.UsuarioService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    
+    @Autowired
+    UsuarioService service;
+
+    @GetMapping()
+    public List<Usuario> getAll() {
+        return service.getAll();
+    }
+}
