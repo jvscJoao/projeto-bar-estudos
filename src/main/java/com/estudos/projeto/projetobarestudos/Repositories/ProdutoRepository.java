7bb1e11d5e55ec4e1db3cb67b8af169c866cfed4
jvscJoao <joaov1500@gmail.com>
2023-12-21 11:27:47 -0300

    Adicionando Produto repository

--- src/main/java/com/estudos/projeto/projetobarestudos/Repositories/ProdutoRepository.java
+++ src/main/java/com/estudos/projeto/projetobarestudos/Repositories/ProdutoRepository.java
@@ -1,9 +1,11 @@
 package com.estudos.projeto.projetobarestudos.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.estudos.projeto.projetobarestudos.domain.Produto.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
     
 }
