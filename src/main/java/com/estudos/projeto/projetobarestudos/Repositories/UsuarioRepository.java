938f1b0e4622777620b65f7574480ea6a657dad8
jvscJoao <joaov1500@gmail.com>
2024-01-02 16:12:20 -0300

    Adicionando findBySituacao para Usuarios

--- src/main/java/com/estudos/projeto/projetobarestudos/Repositories/UsuarioRepository.java
+++ src/main/java/com/estudos/projeto/projetobarestudos/Repositories/UsuarioRepository.java
@@ -1,11 +1,17 @@
-package com.estudos.projeto.projetobarestudos.Repositories;
+package com.estudos.projeto.projetobarestudos.repositories;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.estudos.projeto.projetobarestudos.domain.Usuario.Usuario;
 
+
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     
+    List<Usuario> findBySituacaoTrue();
+
 }
