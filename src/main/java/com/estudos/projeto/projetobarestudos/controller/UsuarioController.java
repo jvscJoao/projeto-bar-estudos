29d1b12455e3a20b445cd33e3f6e5734e170850e
jvscJoao <joaov1500@gmail.com>
2024-01-02 16:14:01 -0300

    Atualizando controller com novo service

--- src/main/java/com/estudos/projeto/projetobarestudos/controller/UsuarioController.java
+++ src/main/java/com/estudos/projeto/projetobarestudos/controller/UsuarioController.java
@@ -1,24 +1,20 @@
 package com.estudos.projeto.projetobarestudos.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.estudos.projeto.projetobarestudos.domain.Usuario.Usuario;
+import com.estudos.projeto.projetobarestudos.dto.UsuarioDTO;
 import com.estudos.projeto.projetobarestudos.services.UsuarioService;
-
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
-
-
 
 @RestController
 @RequestMapping("/usuarios")
@@ -28,24 +24,24 @@ public class UsuarioController {
     UsuarioService service;
 
     @GetMapping()
-    public List<Usuario> getAll() {
-        return service.getAll();
+    public ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> usuarios = service.getAllUsersActive();
+        return ResponseEntity.ok().body(usuarios);
     }
 
     @PostMapping()
-    public Usuario insert(@RequestBody Usuario usuario) {
+    public Usuario insert(@RequestBody @Valid UsuarioDTO usuario) {
         return service.insert(usuario);
     }
-
     
-    @PutMapping()
-    public Usuario update(@RequestBody Usuario usuario) {
-        service.insert(usuario);
-        return usuario;
+    @PutMapping("/{id}")
+    public Usuario update(@PathVariable int id, @RequestBody UsuarioDTO dto) {
+        return service.updateById(id, dto);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable int id) {
-        service.delete(id);
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
